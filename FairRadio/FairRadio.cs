c144f6c4d505768fe911c8d0153a9103455f5eec
costin88boss <42942503+costin88boss@users.noreply.github.com>
2021-01-26 17:42:11 +0200

    Add files

---
+++ FairRadio/FairRadio.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fair_Radio
+{
+    [HarmonyPatch(typeof(Radio), nameof(Radio.UseBattery))]
+    internal static class FairRadio
+    {
+        static bool Prefix(Radio __instance)
+        {
+            if (__instance.isVoiceChatting || __instance.isTransmitting)
+                return true;
+            else return false;
+        }
+    }
+}
---
+++ FairRadio/Main.cs
@@ -0,0 +1,50 @@
+﻿using Exiled.API.Features;
+using Exiled.Loader;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fair_Radio
+{
+    
+    class Main : Plugin<Config>
+    {
+
+        private int _PatchesCounter;
+        Harmony Harmony { get; set; }
+
+        void Patch()
+        {
+            try
+            {
+                Harmony = new Harmony($"Fair_Radio{++_PatchesCounter}");
+                var Lastdebugstatus = Harmony.DEBUG;
+                Harmony.DEBUG = true;
+                Harmony.PatchAll();
+                Harmony.DEBUG = Lastdebugstatus;
+                Log.Debug("applied patch", Loader.ShouldDebugBeShown);
+            }
+            catch (Exception e)
+            {
+                Log.Debug("ERROR, COULDN'T PATCH: " + e);
+            }
+        }
+        void UnPatch()
+        {
+            Harmony.UnpatchAll();
+            Log.Debug("unapplied patch", Loader.ShouldDebugBeShown);
+        }
+
+        public override void OnEnabled()
+        {
+            Patch();
+        }
+        public override void OnDisabled()
+        {
+            UnPatch();
+        }
+    }
+}
